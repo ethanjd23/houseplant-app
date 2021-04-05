a01e4b061148f0b77673d8660a422bfa82d2f4a3
Austin Mann <austin.mann1113@gmail.comgit config --global user.name Austin>
2021-04-05 10:37:18 -0500

    Login

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,22 +1,69 @@
 import * as React from 'react';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Main from './Main';
+import { useState, useEffect } from 'react';
 
-const persistedState = {};
-const store = createStore(
-    // reducers,
-    persistedState,
-    applyMiddleware(reduxThunk))
-const App = () => (
-    <Provider store={store} className="app">
-        <main>
-            <Header />
-            <Main />
-            <Footer />
-        </main>
-    </Provider>
-)
+
+/* HOOK REACT EXAMPLE */
+const App = (props: AppProps) => {
+	const [greeting, setGreeting] = useState<string>('');
+
+	useEffect(() => {
+		async function getGreeting() {
+			try {
+				const res = await fetch('/api/hello');
+				const greeting = await res.json();
+				setGreeting(greeting);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getGreeting();
+	}, []);
+
+	return (
+		<main className="container my-5">
+			<h1 className="text-primary text-center">This is the {greeting} branch!</h1>
+		</main>
+	);
+};
+
+interface AppProps {}
+
+/* CLASS REACT EXAMPLE */
+// class App extends React.Component<IAppProps, IAppState> {
+// 	constructor(props: IAppProps) {
+// 		super(props);
+// 		this.state = {
+// 			name: null
+// 		};
+// 	}
+
+// 	async componentDidMount() {
+// 		try {
+// 			let r = await fetch('/api/hello');
+// 			let name = await r.json();
+// 			this.setState({ name });
+// 		} catch (error) {
+// 			console.log(error);
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+// 			<main className="container my-5">
+// 				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+// 			</main>
+// 		);
+// 	}
+// }
+
+// export interface IAppProps {}
+
+// export interface IAppState {
+// 	name: string;
+// }
+
 export default App;
+
+
 
 
