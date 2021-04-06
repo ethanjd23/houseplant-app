52c8d71de9fd75d06885f71ad93f5e95181be48e
ethanjd23 <ethanjd23@gmail.com>
2021-04-06 14:02:36 -0500

    sql changes and fixed auth error

--- src/server/routes/api/userplants.ts
+++ src/server/routes/api/userplants.ts
@@ -1,20 +1,18 @@
 import * as express from "express";
 import { authenticate } from "passport";
 
-import db from '../../db';
-
+import db from "../../db";
 
 const router = express.Router();
 
-router.get('/id', async (req, res) => {
-    let userid = Number(req.params.id);
-    try {
-        res.json(await db.userplantsDB.getUserPlants(userid));
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-})
-
+router.get("/:id", authenticate("jwt"), async (req, res) => {
+  let userid = Number(req.params.id);
+  try {
+    res.json(await db.userplantsDB.getUserPlants(userid));
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 export default router;
