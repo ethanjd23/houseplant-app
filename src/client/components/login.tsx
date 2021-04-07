0e042e7cd668eeef6851647699123796298c988d
ethanjd23 <ethanjd23@gmail.com>
2021-04-07 11:13:54 -0500

    login cleaned up and redirecting to home on success

--- src/client/components/login.tsx
+++ src/client/components/login.tsx
@@ -6,7 +6,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import Button from '@material-ui/core/Button';
-import $ from 'jquery'
+import $ from 'jquery';
+import { RouteComponentProps } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -95,7 +96,7 @@ const reducer = (state: State, action: Action): State => {
   }
 }
 
-const Login = () => {
+const Login: React.FunctionComponent<RouteComponentProps> = (props) => {
   const classes = useStyles();
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -113,32 +114,19 @@ const Login = () => {
     }
   }, [state.username, state.password]);
 
-//   const handleLogin = () => {
-//     if (state.username === 'abc@email.com' && state.password === 'password') {
-//       dispatch({
-//         type: 'loginSuccess',
-//         payload: 'Login Successfully'
-//       });
-//     } else {
-//       dispatch({
-//         type: 'loginFailed',
-//         payload: 'Incorrect username or password'
-//       });
-//     }
-//   };
-
   const handleLogin = async () => {
       let user = {
         email: state.username,
         password: state.password
-
       }
     $.ajax({
         type: "POST",
         url: "/auth/login",
         data: JSON.stringify(user),
         contentType: "application/json"
-    }).then(response => {console.log(response)
+    }).then(response => {
+      console.log(response)
+      props.history.push("/");
     }
     )};
 
