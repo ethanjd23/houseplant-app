950e65dd44285d854778317055eeacb45c80f14d
MichaJu <Micahjuwallace@gmail.com>
2021-04-09 14:47:58 -0500

    temp merge for pull from main

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,6 +6,11 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Forum from './pages/Forum';
 import Schedule from './pages/Schedule';
+import Request from './pages/Request';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import HomeHeader from './components/Header';
+import HomePlantInfo from './components/HomePlantInfo';
 
 const App = () => {
 	return (
@@ -14,6 +19,7 @@ const App = () => {
 				<Route path='/' exact component={Home} />
 				<Route path='/forum' component={Forum} />
 				<Route path='/schedule' component={Schedule} />
+				<Route path='/request' component={Request} />
 				<Route exact path="/login">
 
 				</Route>
@@ -21,5 +27,25 @@ const App = () => {
 		</BrowserRouter>
 	);
 };
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  minHeight: '100vh',
+	  backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/homepic3.jpg'})`,
+	  backgroundRepeat: 'no-repeat',
+	  backgroundSize: 'cover',
+	},
+  }));
+
+  export default function App() {
+	const classes = useStyles();
+	return (
+	  <div className={classes.root}>
+		<CssBaseline />
+		<HomeHeader />
+		<HomePlantInfo />
+	  </div>
+	);
+  }
 
 export default App;
