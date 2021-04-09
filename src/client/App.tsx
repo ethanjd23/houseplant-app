426dfbd90c99743ec727c8d25d5e353b55e964dd
ethanjd23 <ethanjd23@gmail.com>
2021-04-09 00:20:10 -0500

    test forum working

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/login";
 import PlantDetails from "./components/plantDetails";
 import Home from "./pages/Home";
-import Forum from "./pages/Forum";
+// import Forum from "./pages/Forum";
 import Schedule from "./pages/Schedule";
+import forumPage from "./components/EthanTestComponents/forumPage";
+import ForumDetails from "./components/EthanTestComponents/ForumDetails";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/forum" component={Forum} />
+        {/* <Route path="/forum" component={Forum} /> */}
         <Route path="/schedule" component={Schedule} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/test/:postid" component={ForumDetails} />
+        <Route exact path="/test" component={forumPage} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/client/components/EthanTestComponents/ForumPost.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ForumPost: React.FunctionComponent<ForumPostProps> = ({ post }) => {
+  return (
+    <div className="card" style={{ width: "18rem" }} key={String(post.id)}>
+      <div className="card-body">
+        <h5 className="card-title">{post.title}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{post.userid}</h6>
+        <p className="card-text">{post.content}</p>
+      </div>
+    </div>
+  );
+};
+
+interface ForumPostProps {
+  post: {
+      id: number;
+    userid: number;
+    plantid: number;
+    title: string;
+    content: string;
+    _created: Date;
+  };
+}
+
+export default ForumPost;
