22354e5b0f081e11a7a3656cc60c825233f9ad13
ethanjd23 <ethanjd23@gmail.com>
2021-04-11 19:14:18 -0500

    home page working except for bg

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -13,6 +13,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundImage: '/assets/homepic2.jpeg',
     [theme.breakpoints.down('md')]: {
       flexDirection: 'column',
     },
@@ -23,36 +24,13 @@ const Home: React.FunctionComponent = (props) => {
   const checked = useWindowPosition('header');
   const classes = useStyles();
 
-  const [places, setPlaces] = React.useState([
-    {
-      title: 'Chinese Money Plant',
-      description:
-        " The Chinese Money Plant is a rare and very hard-to-find indoor plant native to southern China. Known for its round pancake-shaped green leaves, the Chinese Money Plant is very popular in modern decor and has become a staple for well-designed interiors.",
-      imageUrl: '/assets/ChineseMoneyPlant.jpg',
-      time: 1500,
-    },
-    {
-      title: 'Calthea Orbifolia',
-      description:
-        'Calathea Orbifolia is an indoor tropical plant known for its stunning striped, olive green leaves that can grow up to six inches wide. The distinguishing features of the thick foliage are patterned leaves with strong light green stripes and a more rounded shape than other Calatheas. This house plant has broad foliage with a rippled effect that gives it an exotic look. It does exceptionally well in low light areas and in order to prevent scorching leaves, it should not be placed in direct sunlight. Calathea orbifolia brings personality and color to any space indoors.',
-      imageUrl: '/assets/Calthea-Orbifolia-.jpg',
-      time: 1500,
-    },
-  ]);
-
   return (
     <>
       <Navbar />
+    <div className={classes.root}>
       <HomeHeader />
-      <div className={classes.root} id="place-to-visit">
-
-        <HomeImageCard place={places[1]} checked={checked} />
-        <HomeImageCard place={places[0]} checked={checked} />
-      </div>
-      <HomePlantInfo />
-      <div className='home'>
-        <h1>Home</h1>
-      </div>
+      <HomePlantInfo />      
+    </div>
     </>
   )
 }
