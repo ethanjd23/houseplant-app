cea71b42d3788c0100c051447b6c4d5cf7199a41
Austin Mann <austin.mann1113@gmail.comgit config --global user.name Austin>
2021-04-12 16:52:41 -0500

    mergin dev

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -16,13 +16,14 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/forum" component={Forum} />
+        <Route exact path="/forum/:userid" component={Forum} />
         <Route exact path="/schedule" component={Schedule} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/test/:postid" component={ForumDetails} />
         <Route exact path="/test" component={forumPage} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/myplants/:userid" component={UserPlants} />
+        
       </Switch>
     </BrowserRouter>
   );
