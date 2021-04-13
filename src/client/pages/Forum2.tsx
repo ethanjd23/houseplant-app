e88e6de1081137702f22a6f03cc3fdbabeabf3f7
ethanjd23 <ethanjd23@gmail.com>
2021-04-13 13:48:35 -0500

    make new post working on forum page

--- src/client/pages/Forum2.tsx
+++ src/client/pages/Forum2.tsx
@@ -7,14 +7,65 @@ import {
 } from "@material-ui/data-grid";
 import ForumPostCard from "../components/forum page/forumPostCard";
 import Navbar from "../components/Navbar";
-import { Button, Container } from "@material-ui/core";
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputLabel, makeStyles, MenuItem, Select, TextField } from "@material-ui/core";
+import { RouteComponentProps } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({  
+    gridRoot: {
+      flexGrow: 1,
+      marginTop: 2,
+      marginLeft: 20,
+      marginRight: 25,
+    },
+    button: {
+      display: "block",
+      marginTop: theme.spacing(2),
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    addButton: {
+      color: "#F50057",
+    }
+  }));
 
 
-const Forum2: React.FunctionComponent = () => {    
-    const [posts, setPosts] = useState([]);    
+const Forum2: React.FunctionComponent<RouteComponentProps> = ({match}) => {  
+    const classes = useStyles();
+    
+    const [posts, setPosts] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [plantOptions, setPlantOptions] = useState([]);
+    const [selectOpen, setSelectOpen] = useState(false);
+    const [plantType, setPlantType] = useState("");
+    const [newTitle, setNewTitle] = useState("");
+    const [newContent, setNewContent] = useState("");
+
+    const handleSelectChange = (event) => {
+        setPlantType(event.target.value);
+      };
+    
+      const handleSelectClose = () => {
+        setSelectOpen(false);
+      };
+    
+      const handleSelectOpen = () => {
+        setSelectOpen(true);
+      };
+    
+
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
 
   useEffect(() => {
       getPosts();
+      getPlantOptions();
   }, []);
 
   return (
@@ -22,20 +73,85 @@ const Forum2: React.FunctionComponent = () => {
     <Navbar />
     <Container>
       {posts.map((post) => <ForumPostCard post={post} />)}
-      <Button variant="contained" color="secondary">Make New Post</Button>
+      <Button onClick={handleOpen} variant="contained" color="secondary">Make New Post</Button>
       </Container>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">New Plant</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please choose the type of plant and its nickname.
+          </DialogContentText>
+          <div>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-controlled-open-select-label">
+                Type
+              </InputLabel>
+              <Select
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                open={selectOpen}
+                onClose={handleSelectClose}
+                onOpen={handleSelectOpen}
+                value={plantType}
+                onChange={handleSelectChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {plantOptions.map((option) => (
+                  <MenuItem key={option.id} value={option.id}>
+                    {option.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="title"
+            label="Title"
+            type="text"
+            fullWidth
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="content"
+            label="Content"
+            type="text"
+            fullWidth
+            onChange={(e) => setNewContent(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={makePost} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 
   async function makePost() {
-      let newPost = {}
+      let newPost = {userid: match.params.userid, plantid: plantType, title: newTitle, content: newContent};
       $.ajax({
           method: "POST",
-          url: `/forum/posts/${post.userid}`,
+          url: `/forum/posts/`,
           data: JSON.stringify(newPost),
           contentType: "application/json"
       }).then(response => {
+          console.log(response);
           getPosts();
+          handleClose();
       }
       )
   }
@@ -43,8 +159,14 @@ const Forum2: React.FunctionComponent = () => {
   async function getPosts() {
     let postRes = await fetch(`/forum/posts/`);
     let post = await postRes.json();
-    setPosts(post);
+    setPosts(post);    
 };
+
+async function getPlantOptions() {
+    let results = await fetch(`/api/plants`);
+    let options = await results.json();
+    setPlantOptions(options);
+  }
 }
 
 export default Forum2;
