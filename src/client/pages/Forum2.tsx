b47796b55daf567697f62a86eb2ce2e3ae3d937c
ethanjd23 <ethanjd23@gmail.com>
2021-04-13 11:32:24 -0500

    working on adding post

--- src/client/pages/Forum2.tsx
+++ src/client/pages/Forum2.tsx
@@ -7,7 +7,7 @@ import {
 } from "@material-ui/data-grid";
 import ForumPostCard from "../components/forum page/forumPostCard";
 import Navbar from "../components/Navbar";
-import { Container } from "@material-ui/core";
+import { Button, Container } from "@material-ui/core";
 
 
 const Forum2: React.FunctionComponent = () => {    
@@ -51,9 +51,23 @@ const Forum2: React.FunctionComponent = () => {
     <Navbar />
     <Container>
       {posts.map((post) => <ForumPostCard post={post} />)}
+      <Button variant="contained" color="secondary">Make New Post</Button>
       </Container>
     </>
   );
+
+  async function makePost() {
+      let newPost = {}
+      $.ajax({
+          method: "POST",
+          url: `/forum/posts/${post.userid}`,
+          data: JSON.stringify(newPost),
+          contentType: "application/json"
+      }).then(response => {
+          getPosts();
+      }
+      )
+  }
 
   async function getPosts() {
     let postRes = await fetch(`/forum/posts/`);
