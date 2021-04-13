17f466936ee6624ef150ac6f4c71f36341e1f4a2
ethanjd23 <ethanjd23@gmail.com>
2021-04-13 13:13:20 -0500

    cleaned up forun2

--- src/client/pages/Forum2.tsx
+++ src/client/pages/Forum2.tsx
@@ -11,40 +11,11 @@ import { Button, Container } from "@material-ui/core";
 
 
 const Forum2: React.FunctionComponent = () => {    
-    const [posts, setPosts] = useState([]);
-    const [rows, setRows] = useState([])
-
-    const rowsTest: GridRowsProp = [
-        { id: 1, username: 'Hello', plantName: 'World', created: 1, content: "idk", title: "idk" },
-        { id: 2, username: 'XGrid', plantName: 'is Awesome', created: 1, content: "idk", title: "idk" },
-        { id: 3, username: 'Material-UI', plantName: 'is Amazing', created: 1, content: "idk", title: "idk" },
-      ];
+    const [posts, setPosts] = useState([]);    
 
   useEffect(() => {
       getPosts();
   }, []);
-
-  const columns: GridColDef[] = [
-    { field: "username", headerName: "Username", width: 130 },
-    { field: "plantName", headerName: "Plant Name", width: 130 },
-    {
-      field: "created",
-      headerName: "Date of Post",      
-      width: 90,
-    },
-    {
-      field: "title",
-      headerName: "Title",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-    },
-    {
-      field: "content",
-      headerName: "Body",
-      type: "text",
-    },
-  ];
 
   return (
     <>
@@ -73,17 +44,6 @@ const Forum2: React.FunctionComponent = () => {
     let postRes = await fetch(`/forum/posts/`);
     let post = await postRes.json();
     setPosts(post);
-    // let postRowArray = await post.map((post) => ({
-    //     id: post.id,
-    //     username: post.username,
-    //     plantName: post.name,
-    //     created: post._created,
-    //     title: post.title,
-    //     content: post.content,
-    //   }))
-    //   console.log(postRowArray);
-    // setRows(postRowArray);
-    
 };
 }
 
